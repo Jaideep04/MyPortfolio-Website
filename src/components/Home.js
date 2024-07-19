f027f4aec551c9531a3c9b1c48b0eae8a5a024b9
Jaideep04 <147320500+Jaideep04@users.noreply.github.com>
2024-07-19 22:57:28 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
-const Footer = () => {
+const Home = () => {
   return (
-    <footer className="py-4 bg-dark text-white-50">
-      <Container className="text-center">
-        <small>&copy; 2024 My Portfolio. All Rights Reserved.</small>
-      </Container>
-    </footer>
+    <Container className="my-5">
+      <Row className="justify-content-center text-center">
+        <Col md={8}>
+          <h1>Welcome to My Portfolio</h1>
+          <p className="lead">
+            Hi, I'm Jaideep Singh ( student at GURU NANAK DEV UNIVERSITY AMRITSAR pursuing B.Tech CSE ), a web developer passionate about creating interactive and user-friendly websites.I also did Cyber Security CC certification.
+          </p>
+          <Button variant="primary" href="#about">Learn more</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
-export default Footer;
-
+export default Home;
